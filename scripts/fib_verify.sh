6aa2f50f06470ff17742df2a68f1a2e2b3891129
Steven HH Chen <s335276@gmail.com>
2020-04-18 15:17:03 +0800

    Change verify script and delete crawler file

--- scripts/fib_verify.sh
+++ scripts/fib_verify.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-rm -rf fibnum.txt
+#rm -rf fibnum.txt
 
-for i in {0..1000}
+for i in {5824..10000}
 do
+    echo $i
     wget -q -O - 127.0.0.1:8081/fib/$i >> fibnum.txt
     echo "" >> fibnum.txt
 done
 
-echo "Fibonacci number get finished..."
+#echo "Fibonacci number get finished..."
 
 # Run specific C file to verify correctness of response fibonacci number
-
+diff fibnum.txt ./scripts/fibnum_crawlar.txt
